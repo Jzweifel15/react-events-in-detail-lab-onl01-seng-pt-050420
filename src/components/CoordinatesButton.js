d66493627b45082fb230b7f7b73c30ede72d50ad
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-14 19:54:46 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
-export default class CoordinatesButton extends Component 
+export default class DelayedButton extends Component 
 {
   render() {
+    let onReceiveCoordinates = (event) => {
+      let coordinates = [];
+      coordinates.push(event.clientX);
+      coordinates.push(event.clientY);
+      return coordinates;
+    }
+
     return (
-      <button></button>
+      <button onClick={this.onReceiveCoordinates}></button>
     )
   }
 }
